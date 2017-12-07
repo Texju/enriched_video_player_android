5ebc324752a565cba87b2ecc666975997dffe78f
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-12-07 09:50:08 +0100

    add vidéo player and stream

--- app/src/main/java/fr/enssat/berrou_couillard/enrichedvideo/MainActivity.java
+++ app/src/main/java/fr/enssat/berrou_couillard/enrichedvideo/MainActivity.java
@@ -2,6 +2,9 @@ package fr.enssat.berrou_couillard.enrichedvideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Vidéo view
+        VideoView vidView = (VideoView)findViewById(R.id.videoView);
+        String vidAddress = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
+        Uri vidUri = Uri.parse(vidAddress);
+        vidView.setVideoURI(vidUri);
+        vidView.start();
+        // Vidéo Control
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+
     }
 }
